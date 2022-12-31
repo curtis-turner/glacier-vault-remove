04a66660e30127c80b02a39332d988c7613e374d
curtis-turner <cjturner714@gmail.com>
2022-12-31 07:30:32 -0800

    create main function to wrap remove_vault function with primary logic, this will make it easier to further separate concerns into dedicated functions

--- remove_vault.py
+++ remove_vault.py
@@ -27,7 +27,7 @@ def process_archive(archive_list):
 				    archiveId=archive['ArchiveId']
 				)
 			except:
-				printException()
+				print_exception()
 
 				logging.info('Sleep 2s before retrying...')
 				time.sleep(2)
@@ -42,7 +42,7 @@ def process_archive(archive_list):
 				except:
 					logging.error('Cannot remove archive ID : %s', archive['ArchiveId'])
 
-def printException():
+def print_exception():
 	exc_type, exc_value = sys.exc_info()[:2]
 	logging.error('Exception "%s" occured with message "%s"', exc_type.__name__, exc_value)
 
@@ -56,177 +56,184 @@ def get_jobs(vaultName):
 
 		return jobs_list
 	except:
-		printException()
+		print_exception()
 		return []
 
-# Default logging config
-logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO, datefmt='%H:%M:%S')
+def remove_vault():
+	# Default logging config
+	logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO, datefmt='%H:%M:%S')
 
-# Get arguments
-if len(sys.argv) >= 3:
-	regionName = sys.argv[1]
-	vaultName = sys.argv[2]
-	numProcess = 1
-	retrievalJob = 'LATEST'
-else:
-	# If there are missing arguments, display usage example and exit
-	logging.error('Usage: %s <region_name> [<vault_name>|LIST] [DEBUG] [NUMPROCESS] [<job_id>|LIST|NEW|LATEST]', sys.argv[0])
-	sys.exit(1)
-
-# 3rd argument - log level, num process or job ID
-if len(sys.argv) >= 4:
-	if sys.argv[3] == 'DEBUG':
-		logging.info('Logging level set to DEBUG.')
-		logging.getLogger().setLevel(logging.DEBUG)
-	elif sys.argv[3].isdigit():
-		numProcess = int(sys.argv[3])
+	# Get arguments
+	if len(sys.argv) >= 3:
+		regionName = sys.argv[1]
+		vaultName = sys.argv[2]
+		numProcess = 1
+		retrievalJob = 'LATEST'
 	else:
-		retrievalJob = sys.argv[3]
-
-# 4th argument - num process or job ID
-if len(sys.argv) >= 5:
-	if sys.argv[4].isdigit():
-		numProcess = int(sys.argv[4])
-	else:
-		retrievalJob = sys.argv[4]
-
-logging.info('Running with %s processes', numProcess)
-
-# 5th argument - job ID
-if len(sys.argv) >= 6:
-	retrievalJob = sys.argv[5]
-
-os.environ['AWS_DEFAULT_REGION'] = regionName
-
-# Load credentials
-try:
-	f = open('credentials.json', 'r')
-	config = json.loads(f.read())
-	f.close()
-
-	os.environ['AWS_ACCESS_KEY_ID'] = config['AWSAccessKeyId']
-	os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWSSecretKey']
-
-except:
-	logging.error('Cannot load "credentials.json" file... Assuming Role Authentication.')
-
-sts_client = boto3.client("sts")
-accountId = sts_client.get_caller_identity()["Account"]
-
-logging.info("Working on AccountID: {id}".format(id=accountId))
-
-try:
-	logging.info('Connecting to Amazon Glacier...')
-	glacier = boto3.client('glacier')
-except:
-	printException()
-	sys.exit(1)
-
-if vaultName == 'LIST':
-	try:
-		logging.info('Getting list of vaults...')
-		response = glacier.list_vaults()
-		vault_list = response.get('VaultList')
-		while response.get('Marker') is not None:
-			response = glacier.list_vaults(marker=response['Marker'])
-			vault_list += response.get('VaultList')
-	except:
-		printException()
+		# If there are missing arguments, display usage example and exit
+		logging.error('Usage: %s <region_name> [<vault_name>|LIST] [DEBUG] [NUMPROCESS] [<job_id>|LIST|NEW|LATEST]', sys.argv[0])
 		sys.exit(1)
 
-	for vault in vault_list:
-		logging.info(vault['VaultName'])
+	# 3rd argument - log level, num process or job ID
+	if len(sys.argv) >= 4:
+		if sys.argv[3] == 'DEBUG':
+			logging.info('Logging level set to DEBUG.')
+			logging.getLogger().setLevel(logging.DEBUG)
+		elif sys.argv[3].isdigit():
+			numProcess = int(sys.argv[3])
+		else:
+			retrievalJob = sys.argv[3]
 
-	exit(0)
+	# 4th argument - num process or job ID
+	if len(sys.argv) >= 5:
+		if sys.argv[4].isdigit():
+			numProcess = int(sys.argv[4])
+		else:
+			retrievalJob = sys.argv[4]
 
-if retrievalJob == 'LIST':
-	logging.info('Getting list of inventory retrieval jobs...')
-	jobs_list = get_jobs(vaultName)
+	logging.info('Running with %s processes', numProcess)
 
-	for job in jobs_list:
-		if job['Action'] == 'InventoryRetrieval':
-			logging.info("{id} - {date} - {status}".format(id=job['JobId'], date=job['CreationDate'], status=job['StatusCode']))
+	# 5th argument - job ID
+	if len(sys.argv) >= 6:
+		retrievalJob = sys.argv[5]
 
-	exit(0)
+	os.environ['AWS_DEFAULT_REGION'] = regionName
 
-try:
-	logging.info('Getting selected vault... [{v}]'.format(v=vaultName))
-	vault = glacier.describe_vault(vaultName=vaultName)
-	logging.info("Working on ARN {arn}".format(arn=vault['VaultARN']))
-except:
-	printException()
-	sys.exit(1)
-
-if retrievalJob == 'LATEST':
-	logging.info('Looking for the latest inventory retrieval job...')
-	jobs_list = get_jobs(vaultName) # Reversed to get the latest, not the first
-	retrievalJob = ''
-
-	# Check if a job already exists
-	for job in jobs_list:
-		if job['Action'] == 'InventoryRetrieval':
-			logging.info('Found existing job...')
-			retrievalJob = job['JobId']
-			break
-	
-	if retrievalJob == '':
-		logging.info('No existing job found...')
-
-if retrievalJob == '' or retrievalJob == 'NEW':
-	logging.info('Initiate inventory retrieval...')
+	# Load credentials
 	try:
-		glacier_resource = boto3.resource('glacier')
-		vault = glacier_resource.Vault(accountId, vaultName)
-		job = vault.initiate_inventory_retrieval()
+		f = open('credentials.json', 'r')
+		config = json.loads(f.read())
+		f.close()
 
-		retrievalJob = job.id
+		os.environ['AWS_ACCESS_KEY_ID'] = config['AWSAccessKeyId']
+		os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWSSecretKey']
+
 	except:
-		printException()
+		logging.error('Cannot load "credentials.json" file... Assuming Role Authentication.')
+
+	sts_client = boto3.client("sts")
+	accountId = sts_client.get_caller_identity()["Account"]
+
+	logging.info("Working on AccountID: {id}".format(id=accountId))
+
+	try:
+		logging.info('Connecting to Amazon Glacier...')
+		glacier = boto3.client('glacier')
+	except:
+		print_exception()
 		sys.exit(1)
 
-logging.info('Job ID : %s', retrievalJob)
+	if vaultName == 'LIST':
+		try:
+			logging.info('Getting list of vaults...')
+			response = glacier.list_vaults()
+			vault_list = response.get('VaultList')
+			while response.get('Marker') is not None:
+				response = glacier.list_vaults(marker=response['Marker'])
+				vault_list += response.get('VaultList')
+		except:
+			print_exception()
+			sys.exit(1)
 
-# Get job status
-job = glacier.describe_job(vaultName=vaultName, jobId=retrievalJob)
+		for vault in vault_list:
+			logging.info(vault['VaultName'])
 
-logging.info('Job Creation Date: {d}'.format(d=job['CreationDate']))
+		exit(0)
 
-while job['StatusCode'] == 'InProgress':
-	# Job are usualy ready within 4hours of request.
-	logging.info('Inventory not ready, sleep for 10 mins...')
+	if retrievalJob == 'LIST':
+		logging.info('Getting list of inventory retrieval jobs...')
+		jobs_list = get_jobs(vaultName)
 
-	time.sleep(60*10)
+		for job in jobs_list:
+			if job['Action'] == 'InventoryRetrieval':
+				logging.info("{id} - {date} - {status}".format(id=job['JobId'], date=job['CreationDate'], status=job['StatusCode']))
 
+		exit(0)
+
+	try:
+		logging.info('Getting selected vault... [{v}]'.format(v=vaultName))
+		vault = glacier.describe_vault(vaultName=vaultName)
+		logging.info("Working on ARN {arn}".format(arn=vault['VaultARN']))
+	except:
+		print_exception()
+		sys.exit(1)
+
+	if retrievalJob == 'LATEST':
+		logging.info('Looking for the latest inventory retrieval job...')
+		jobs_list = get_jobs(vaultName) # Reversed to get the latest, not the first
+		retrievalJob = ''
+
+		# Check if a job already exists
+		for job in jobs_list:
+			if job['Action'] == 'InventoryRetrieval':
+				logging.info('Found existing job...')
+				retrievalJob = job['JobId']
+				break
+		
+		if retrievalJob == '':
+			logging.info('No existing job found...')
+
+	if retrievalJob == '' or retrievalJob == 'NEW':
+		logging.info('Initiate inventory retrieval...')
+		try:
+			glacier_resource = boto3.resource('glacier')
+			vault = glacier_resource.Vault(accountId, vaultName)
+			job = vault.initiate_inventory_retrieval()
+
+			retrievalJob = job.id
+		except:
+			print_exception()
+			sys.exit(1)
+
+	logging.info('Job ID : %s', retrievalJob)
+
+	# Get job status
 	job = glacier.describe_job(vaultName=vaultName, jobId=retrievalJob)
 
-if job['StatusCode'] == 'Succeeded' and __name__ == '__main__':
-	logging.info('Inventory retrieved, parsing data...')
-	job_output = glacier.get_job_output(vaultName=vaultName, jobId=job['JobId'])
-	inventory = json.loads(job_output['body'].read().decode('utf-8'))
+	logging.info('Job Creation Date: {d}'.format(d=job['CreationDate']))
 
-	archiveList = inventory['ArchiveList']
+	while job['StatusCode'] == 'InProgress':
+		# Job are usualy ready within 4hours of request.
+		logging.info('Inventory not ready, sleep for 10 mins...')
 
-	logging.info('Removing %s archives... please be patient, this may take some time...', len(archiveList));
-	archiveParts = split_list(archiveList, numProcess)
-	jobs = []
+		time.sleep(60*10)
 
-	for archive in archiveParts:
-		p = Process(target=process_archive, args=(archive,))
-		jobs.append(p)
-		p.start()
+		job = glacier.describe_job(vaultName=vaultName, jobId=retrievalJob)
 
-	for j in jobs:
-		j.join()
+	if job['StatusCode'] == 'Succeeded' and __name__ == '__main__':
+		logging.info('Inventory retrieved, parsing data...')
+		job_output = glacier.get_job_output(vaultName=vaultName, jobId=job['JobId'])
+		inventory = json.loads(job_output['body'].read().decode('utf-8'))
 
-	logging.info('Removing vault...')
-	try:
-		glacier.delete_vault(
-		    vaultName=vaultName
-		)
-		logging.info('Vault removed.')
-	except:
-		printException()
-		logging.error('We cant remove the vault now. Please wait some time and try again. You can also remove it from the AWS console, now that all archives have been removed.')
+		archiveList = inventory['ArchiveList']
 
-else:
-	logging.info('Vault retrieval failed.')
+		logging.info('Removing %s archives... please be patient, this may take some time...', len(archiveList));
+		archiveParts = split_list(archiveList, numProcess)
+		jobs = []
+
+		for archive in archiveParts:
+			p = Process(target=process_archive, args=(archive,))
+			jobs.append(p)
+			p.start()
+
+		for j in jobs:
+			j.join()
+
+		logging.info('Removing vault...')
+		try:
+			glacier.delete_vault(
+				vaultName=vaultName
+			)
+			logging.info('Vault removed.')
+		except:
+			print_exception()
+			logging.error('We cant remove the vault now. Please wait some time and try again. You can also remove it from the AWS console, now that all archives have been removed.')
+
+	else:
+		logging.info('Vault retrieval failed.')
+
+def main():
+	remove_vault()
+
+if __name__ == "__main__":
+	main()
